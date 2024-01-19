ec3dc584000d7a2057aab4ade56ada90eb2d41f2
Matheus Reis <matheuss_reis@outlook.com>
2024-01-19 13:43:32 -0300

    Upload Single Server Project

---
+++ phoenix_single/bin/phoenix_single.dart
@@ -0,0 +1,5 @@
+import 'package:phoenix_single/phoenix_single.dart' as phoenix_single;
+
+void main(List<String> arguments) {
+  print('Hello world: ${phoenix_single.calculate()}!');
+}
---
+++ phoenix_single/test/phoenix_single_test.dart
@@ -0,0 +1,8 @@
+import 'package:phoenix_single/phoenix_single.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

ee0a8f4763699dd95665e983876d20231cc346ea
Matheus Reis <matheuss_reis@outlook.com>
2024-01-19 10:21:00 -0300

    Upload do servidor de chat

---
+++ phoenix_chat/bin/phoenix_chat.dart
@@ -0,0 +1,5 @@
+import 'package:phoenix_chat/phoenix_chat.dart' as phoenix_chat;
+
+void main(List<String> arguments) {
+  print('Hello world: ${phoenix_chat.calculate()}!');
+}
---
+++ phoenix_chat/test/phoenix_chat_test.dart
@@ -0,0 +1,8 @@
+import 'package:phoenix_chat/phoenix_chat.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

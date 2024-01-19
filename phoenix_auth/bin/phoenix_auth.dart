1bcc3eef570169ce34352346323f8104ecc43c61
Matheus Reis <matheuss_reis@outlook.com>
2024-01-19 10:20:44 -0300

    Upload do servidor de autenticação

---
+++ phoenix_auth/bin/phoenix_auth.dart
@@ -0,0 +1,5 @@
+import 'package:phoenix_auth/phoenix_auth.dart' as phoenix_auth;
+
+void main(List<String> arguments) {
+  print('Hello world: ${phoenix_auth.calculate()}!');
+}
---
+++ phoenix_auth/test/phoenix_auth_test.dart
@@ -0,0 +1,8 @@
+import 'package:phoenix_auth/phoenix_auth.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}

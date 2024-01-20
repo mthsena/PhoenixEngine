dc78fad90d2276f89d4f30aaeb8efb718294975e
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2024-01-20 08:59:16 -0300

    Testes do DataHandler

---
+++ phoenix_single/test/network/data_handler_test.dart
@@ -0,0 +1,24 @@
+import 'package:phoenix_single/data/packet/client_packets.dart';
+import 'package:phoenix_single/network/data_handler/data_handler.dart';
+import 'package:phoenix_single/network/data_handler/message/placeholder_handle.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('DataHandler', () {
+    late DataHandler dataHandler;
+
+    setUp(() {
+      dataHandler = DataHandler();
+    });
+
+    test('inicializa handleDataMessage corretamente', () {
+      expect(dataHandler.handleDataMessage.length, equals(ClientPackets.values.length));
+      expect(dataHandler.handleDataMessage[0], isA<PlaceholderHandler>());
+      // Verifique outros índices conforme necessário
+    });
+
+    test('handleData lança exceção para msgType fora do intervalo', () {
+      expect(() => dataHandler.handleData(index: 0, data: [ClientPackets.values.length + 1, 0, 0, 0]), throwsRangeError);
+    });
+  });
+}
